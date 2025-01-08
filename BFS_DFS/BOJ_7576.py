44e74e37a54d09a48a51e97f24468f63dc59f70c
Hyuns66 <hyuns6677@gmail.com>
2025-01-08 12:25:39 +0900

    boj_7576

---
+++ BFS_DFS/BOJ_7576.py
@@ -0,0 +1,50 @@
+import sys
+from collections import deque
+
+def day_one(graph, q):
+    dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    new_q = deque()
+    while q:
+        y, x = q.popleft()
+        for dy, dx in dirs:
+            py = y + dy
+            px = x + dx
+            if py < 0 or px < 0 or py >= len(graph) or px >= len(graph[0]):
+                continue
+            if graph[py][px] != 0:
+                continue
+            graph[py][px] = 1
+            new_q.append((py, px))
+    return new_q
+
+def check(graph):
+    for g in graph:
+        for tomato in g:
+            if tomato == 0:
+                return False
+    return True
+
+M, N = map(int, input().split())
+graph = list()
+q = deque()
+for y in range(N):
+    temp = list(map(int, input().split()))
+    for x, tomato in enumerate(temp):
+        if tomato == 1:
+            q.append((y, x))
+    graph.append(temp)
+
+answer = 0
+while True:
+    q = day_one(graph, q.copy())
+    # 더이상 익을수 있는 토마토가 없으면 종료
+    # 토마토가 하나라도 익었으면 day + 1
+    if len(q) == 0:
+        break
+    else:
+        answer += 1
+
+if not check(graph):
+    print(-1)
+else:
+    print(answer)
