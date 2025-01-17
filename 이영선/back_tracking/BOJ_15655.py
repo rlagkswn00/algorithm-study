41ea78cd969cb9e05fabd95a0d7b384f2e8ce66a
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-01-17 11:03:42 +0900

    Add BackTracking - N과 M

---
+++ 이영선/back_tracking/BOJ_15655.py
@@ -0,0 +1,19 @@
+# 백트래킹 - 15655번 - N과 M (6)
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+def bt(result):
+    global m
+    if len(result) == m:
+        print(' '.join(map(str,result)))
+        return
+    for i in arr:
+        if len(result) == 0 or result[-1] < i:
+            result.append(i)
+            bt(result)
+            result.pop(-1)
+bt([])
